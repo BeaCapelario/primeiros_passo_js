948aa0c9239dfc4c8f82ef8b9ac846fbc76ecd31
Beatriz Feitosa Capelario <42400785805@>
2025-08-14 08:17:09 -0300

    first

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+// //alert : Print
+// alert("Olá DS19")
+
+// //prompt : Input
+// prompt("Qual seu nome?")
+
+// //confirm : Te da duas opções Ok e Cancel
+// confirm("É seu nome mesmo?")
+
+// //let: variáveis com valores flexiveis
+// let nome = prompt("Qual o seu nome?")
+// alert("Bem vinda," + nome)
+
+// //const: constantes, com valores fixos
+
+alert("Olá, usuário!")
+let nome = prompt("Me fale o seu nome:")
+confirm("É seu nome mesmo?")
+alert(`Olá! ${nome}, seja bem-vindo(a)`)
+
+let materia = prompt("Agora me fale a sua matéria preferida:")
+alert(`A matéria ${materia} é realmente é ótima!`)
+
+prompt("Qual a data de hoje?")
+
+confirm("Está preparado para o segundos semestre de Desenvolvimento de Sistemas?")
+
